aaf68e1fbe77cc74a896d36c4d8dc5d2b35d97d2
z000dh2o <iunknown.venkat@gmail.com>
2021-02-02 23:21:34 +0530

    Ok

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ app = FastAPI(title=title,description=description,version=version)
 if __name__ == "__main__":
 	uvicorn.run(app,
 					host="0.0.0.0",
-					port=8000)
+					port=8080)
 
 class SumNumbers(BaseModel):
     first_number: int = Field(None)
