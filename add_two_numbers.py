1557cf93228c88288337c65d6d8b50eb28557112
z000dh2o <iunknown.venkat@gmail.com>
2021-02-02 22:17:00 +0530

    Sathish's code

---
+++ add_two_numbers.py
@@ -0,0 +1,3 @@
+def sum_two_numbers(num1, num2):
+	sum = int(num1) + int(num2)
+	return sum
---
+++ api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from starlette.responses import JSONResponse
+from add_two_numbers import sum_two_numbers
+
+title="REST API methods for performing addition of two numbers"
+version="1.0"
+description="Interface Specification for adding two numbers"
+
+app = FastAPI(title=title,description=description,version=version)
+
+if __name__ == "__main__":
+	uvicorn.run(app,
+					host="0.0.0.0",
+					port=8000)
+
+class SumNumbers(BaseModel):
+    first_number: int = Field(None)
+    second_number: int = Field(None)
+
+@app.post("/api/v1/operations/sum_numbers")
+def sum_numbers(post_request_data: SumNumbers):
+    return JSONResponse(status_code=200, content=sum_two_numbers(SumNumbers.first_number, SumNumbers.second_number))
